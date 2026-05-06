53ceaa3ea640b9906c9a044805ed3e523a0971f2
Divyansh singh <ffshiva494@gmail.com>
2026-05-06 16:46:24 +0530

    commit

--- video 85/server.js
+++ video 85/server.js
@@ -1,1 +1,9 @@
-console.log("BMW")
+// console.log("BMW")
+
+var slugify = require('slugify')
+
+let a = slugify('some string') // some-string
+console.log(a)
+// if you prefer something other than '-' as separator
+const b= slugify('some string', '_')  // some_string
+console.log(b)
