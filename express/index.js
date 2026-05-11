39f01f0d1cfab1a78fbe8ded7252c3ca5cde2cee
Divyansh singh <ffshiva494@gmail.com>
2026-05-11 09:25:08 +0530

    commit

--- express/index.js
+++ express/index.js
@@ -7,12 +7,40 @@ let port =8080;
 
 app.listen(port, () =>{
     console.log('app is listening on port ${ports}');
+
+    
 });
-app.use((req, res) =>{
-    // console.log(req);
-    console.log("request  received");
-    res.send({
-        name : "AD",
-        color:"red",
-    });
+
+app.get("/",(req,res)=>{
+    res.send("this is first page");
 });
+
+// app.get("/:username",(req,res)=>{
+//     let htmlStr = '<h1> Welcome to the page of @${username}</h1>'
+//     // console.log(req.params);
+//     res.send(htmlStr);
+// });
+
+
+app.get("/search",(req,res)=>{
+
+    let { q} = req.query;
+    res.send('this result:${q}');
+});
+
+
+
+
+
+
+
+// app.use((req, res) =>{
+//     // console.log(req);
+//     console.log("request  received");
+//     res.send({
+//         name : "AD",
+//         color:"red",
+//     });
+// });
+
+
